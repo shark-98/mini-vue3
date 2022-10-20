a9f8da5ed73fcd1815358309c5878636abd44817
likangbo <67063670@qq.com>
2022-10-21 00:46:54 +0800

    feat: add effect scheduler

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,7 +1,7 @@
 class ReactiveEffect {
   private _fn
 
-  constructor(fn: Function) {
+  constructor(fn: Function, public scheduler?: Function) {
     this._fn = fn
   }
 
@@ -32,12 +32,16 @@ export const trigger = (target: any, key: any) => {
   const targetDep = targetMap.get(target)
   const deps = targetDep.get(key)
   for (const dep of deps) {
-    dep.run()
+    if (dep.scheduler) {
+      dep.scheduler()
+    } else {
+      dep.run()
+    }
   }
 }
 
-export const effect = (fn: Function) => {
-  const _effect = new ReactiveEffect(fn)
+export const effect = (fn: Function, options: any = {}) => {
+  const _effect = new ReactiveEffect(fn, options.scheduler)
   _effect.run()
   return _effect.run.bind(_effect)
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -35,4 +35,39 @@ describe('effect', () => {
     expect(a).toBe(3)
     expect(r).toBe('hello effect runner')
   })
+
+  it('scheduler', () => {
+    let x = 0
+    const obj = reactive({ a: 1 })
+
+    let run: Function = () => { }
+    const scheduler = jest.fn(() => {
+      run = runner
+    })
+    const runner = effect(
+      () => {
+        x = obj.a
+      },
+      { scheduler }
+    )
+
+    // init
+    expect(x).toBe(1)
+    expect(scheduler).not.toHaveBeenCalled()
+
+    // update
+    obj.a++
+    expect(x).toBe(1)
+    expect(scheduler).toHaveBeenCalledTimes(1)
+
+    // call update
+    run()
+    expect(x).toBe(2)
+    expect(scheduler).toHaveBeenCalledTimes(1)
+
+    // update
+    obj.a++
+    expect(x).toBe(2)
+    expect(scheduler).toHaveBeenCalledTimes(2)
+  })
 })
